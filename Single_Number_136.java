0ef5e03e6f9aef04beb136fe5a1aec4702c48b45
Leo <1060907970@qq.com>
2017-07-14 14:57:28 +0800

    Update Single_Number_136.java

--- Single_Number_136.java
+++ Single_Number_136.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
    Created by ljx on 17-7-14.
    Given an array of integers, every element appears twice except for one. Find that single one.
@@ -8,11 +11,27 @@
 public class Single_Number_136 {
     public Single_Number_136() {}
 
+    /**
+     * 方案１:
+     * 由于数组中除了一个元素是single之外，其余元素均出现两次，
+     * 这里设置一个set，对于每个元素判断是否在集合中，若不存在，则添加；若存在，则删除。
+     * 那么，集合中剩余的最后一个元素就是single。
+     */
     public int singleNumber(int[] nums) {
-        
+        Set set = new HashSet<Integer>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                set.remove(num);
+            } else {
+                set.add(num);
+            }
+        }
+        return (int)set.iterator().next();
     }
 
     public static void main(String[] args) {
-
+        int[] nums = {1,2,2,3,1,3,0,4,4};
+        int single = new Single_Number_136().singleNumber(nums);
+        System.out.println(single);
     }
 }
