a9b4aba1768e699d5a30414fc14369f66b6ec447
Leo <1060907970@qq.com>
2017-07-14 14:24:36 +0800

    Update Single_Number_136.java

--- Single_Number_136.java
+++ Single_Number_136.java
@@ -1,3 +1,10 @@
+/**
+   Created by ljx on 17-7-14.
+   Given an array of integers, every element appears twice except for one. Find that single one.
+
+   Note:
+   Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ */
 public class Single_Number_136 {
     public Single_Number_136() {}
 
