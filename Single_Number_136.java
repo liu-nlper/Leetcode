6287329cf64355b51a3805b0b4bf4e1c43df7e1c
Leo <1060907970@qq.com>
2017-07-14 14:59:23 +0800

    Update Single_Number_136.java

--- Single_Number_136.java
+++ Single_Number_136.java
@@ -2,17 +2,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
-   Created by ljx on 17-7-14.
+
+   问题描述：
+
    Given an array of integers, every element appears twice except for one. Find that single one.
 
    Note:
    Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
  */
 public class Single_Number_136 {
     public Single_Number_136() {}
 
     /**
-     * 方案１（该方案需要额外的空间）:
+     * 方案１（该方案需要额外的memory）:
      * 由于数组中除了一个元素是single之外，其余元素均出现两次，
      * 这里设置一个set，对于每个元素判断是否在集合中，若不存在，则添加；若存在，则删除。
      * 那么，集合中剩余的最后一个元素就是single。
